f1f4769e3ecf08b1f3f61fe6881e42b27c016700
KethraPackiamB <kethrapackiamb@gmail.com>
2025-06-08 13:52:38 +0530

    CRM project files are adding to the repository

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Customers } from './CRM/Customers';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Customers/>
   </React.StrictMode>
 );
 
